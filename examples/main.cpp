f199cf5cc853a2b9d0eec7daddecf4d3af015f9c
NoahSch1999 <77342033+NoahSch1999@users.noreply.github.com>
2026-03-14 21:29:20 +0100

    Added audio files and example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-#include "MediumAudio.hpp"
+#include "include/MediumAudio.hpp"
+#include <Windows.h>
+using namespace aZero;
 
 // API EXAMPLE
 int main(int argc, char* argv[]) {
 	
+	Audio::AudioSystem audioSystem;
+	Audio::AudioSource source;
 
+	source = audioSystem.LoadAudio("C:/Users/Noah Schierenbeck/Music/test.wav").value();
+	source.Play();
+
+	Sleep(10000);
 
 	return 0;
 }
